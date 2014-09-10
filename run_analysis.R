2884f56b007ce5044941e25e45faa9c9c3182882
jmlivingstone <julie.marie.livingstone@gmail.com>
2014-09-10 18:55:17 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # download data
-file <- paste0(Sys.Date(), final_project_date.zip")
+file <- paste0(Sys.Date(), "final_project_date.zip")
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", file, method="curl")
 cmd <- paste("unzip", file)
 system(cmd)
